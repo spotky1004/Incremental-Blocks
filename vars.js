927083b2879a3491d4aa367f3406dd666c1e32c7
Spotky1004 <spotky1004@gmail.com>
2020-06-27 00:17:21 +0900

    0.6.2

--- vars.js
+++ vars.js
@@ -380,7 +380,7 @@ boostShortName = [
   'bpc', 'bps', 'bupc', 'bu eff', 'tok p', 'tok m'
 ];
 boostRange = [
-  [2, 3], [2, 3], [3, 10], [2, 4], [2, 3], [2, 3]
+  [2, 3], [2, 3], [3, 10], [2, 4], [2, 3], [1.5, 2]
 ];
 boostLongName = [
   'bpc mult', 'bps mult', 'bupc mult', 'bupc effect', 'token %', 'token mult'
